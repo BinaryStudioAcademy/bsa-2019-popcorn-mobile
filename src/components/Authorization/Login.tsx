611a9008672dd261847fc6c25fda4f26eeb286c5
Uliana Lobanova <lobanova.ulianaa@gmail.com>
2019-08-19 18:36:39 +0300

    authorization: add validation and fix styles

--- src/components/Authorization/Login.tsx
+++ src/components/Authorization/Login.tsx
@@ -6,6 +6,7 @@ import {
   View,
   StyleSheet,
 } from 'react-native';
+import SvgUri from 'react-native-svg-uri';
 
 interface IProps {
     navigation: any
@@ -13,7 +14,11 @@ interface IProps {
 
 interface IState {
     email: string,
-    password: string
+    password: string,
+    emailError: string,
+    passwordError: string,
+    emailFocus: boolean,
+    passwordFocus: boolean,
 }
 
 export default class Login extends Component<IProps, IState> {
@@ -23,6 +28,10 @@ export default class Login extends Component<IProps, IState> {
     this.state = {
       email: '',
       password: '',
+      emailError: '',
+      passwordError: '',
+      emailFocus: false,
+      passwordFocus: false,
     };
   }
 
@@ -40,31 +49,97 @@ export default class Login extends Component<IProps, IState> {
     this.props.navigation.navigate('Signup');
   };
 
+  onEmailFocus = () => {
+    this.setState({ emailError: '' });
+  }
+
+  onPasswordFocus = () => {
+    this.setState({ passwordError: '' });
+  }
+
+  validateEmail = () => {
+    const { email } = this.state;
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!re.test(String(email).toLowerCase())) 
+    this.setState({ emailError: 'Email is invalid' });
+    if (email.trim() === '') 
+    this.setState({ emailError: 'Email is required' });
+  }
+
+  validatePassword = () => {
+    const { password } = this.state;
+    if (password.length < 6) 
+    this.setState({ passwordError: 'Password must be at least 6 characters' });
+    if (password.trim() === '')
+    this.setState({ passwordError: 'Password is required' })
+  }
+
   render() {
-    const {password, email} = this.state;
+    const { 
+      password, 
+      email, 
+      emailError, 
+      passwordError,
+      passwordFocus,
+      emailFocus
+    } = this.state;
     return (
       <View style={styles.container}>
         <Text style={[ styles.text, styles.header ]}>Welcome back!</Text>
         <TextInput
-          style={[ styles.text, styles.input ]}
+          style={[ 
+            styles.text, 
+            styles.input, 
+            !!emailError && styles.borderError,
+            emailFocus && styles.inputFocus
+        ]}
           placeholder="Email address"
           value={email}
           onChangeText={this.onChangeEmail}
+          onBlur={() => {
+            this.validateEmail();
+            this.setState({ emailFocus: false })
+          }}
+          onFocus={() => {
+            this.setState({ emailError: '', emailFocus: true });
+          }}
         />
+        {
+            !!emailError &&
+            <Text style={[ styles.text, styles.error ]}>{emailError}</Text>
+        }
         <TextInput
-          style={[ styles.text, styles.input ]}
+          style={[ 
+            styles.text, 
+            styles.input, 
+            !!passwordError && styles.borderError,
+            passwordFocus && styles.inputFocus
+        ]}
           placeholder="Password"
           secureTextEntry={true}
           value={password}
           onChangeText={this.onChangePassword}
+          maxLength={64}
+          onBlur={() => { 
+            this.validatePassword();
+            this.setState({ passwordFocus: false });
+          }}
+          onFocus={() => { 
+            this.setState({ passwordError: '', passwordFocus: true }); 
+          }}
         />
+        {
+            !!passwordError &&
+            <Text style={[ styles.text, styles.error ]}>{passwordError}</Text>
+        }
         <TouchableOpacity onPress={this.onPress}>
           <Text style={[ styles.text, styles.button ]}>Sign in</Text>
         </TouchableOpacity>
         <View style={styles.message}>
           <Text style={[ styles.text, styles.secondaryText]}>Don't have an account? </Text>
-          <TouchableOpacity onPress={this.onSignup}>
+          <TouchableOpacity onPress={this.onSignup} style={styles.linkContainer}>
             <Text style={[styles.text, styles.link]}>Register </Text>
+            <SvgUri source={require('../../assets/general/arrow.svg')} />
           </TouchableOpacity>
         </View>
       </View>
@@ -86,7 +161,7 @@ const styles = StyleSheet.create({
   },
   input: {
     width: '80%',
-    margin: 10,
+    marginBottom: 20,
     borderColor: 'rgba(0, 0, 0, 0.11)',
     borderWidth: 1,
     padding: 12,
@@ -119,6 +194,22 @@ const styles = StyleSheet.create({
   },
   link: {
     textTransform: 'uppercase',
-    fontFamily: 'Inter-SemiBold'
+    fontFamily: 'Inter-SemiBold',
+    marginRight: 5
+  },
+  linkContainer: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  borderError: {
+    borderColor: 'rgba(255, 0, 0, .6)',
+    marginBottom: 1,
+    borderWidth: 1,
+  },
+  error: {
+    color: 'rgba(255, 0, 0, .6)',
+  },
+  inputFocus: {
+    borderColor: 'rgba(0, 0, 0, 0.4)',
   }
 });
--- src/components/Authorization/Signup.tsx
+++ src/components/Authorization/Signup.tsx
@@ -6,6 +6,7 @@ import {
   TextInput,
   StyleSheet,
 } from 'react-native';
+import SvgUri from 'react-native-svg-uri';
 
 interface IProps {
     navigation: any
@@ -14,7 +15,13 @@ interface IProps {
 interface IState {
     email: string,
     name: string,
-    password: string
+    password: string,
+    passwordError: string,
+    emailError: string,
+    nameError: string,
+    emailFocus: boolean,
+    passwordFocus: boolean,
+    nameFocus: boolean
 }
 
 export default class Signup extends Component<IProps, IState> {
@@ -25,6 +32,12 @@ export default class Signup extends Component<IProps, IState> {
       email: '',
       password: '',
       name: '',
+      passwordError: '',
+      emailError: '',
+      nameError: '',
+      emailFocus: false,
+      passwordFocus: false,
+      nameFocus: false
     };
   }
 
@@ -44,39 +57,125 @@ export default class Signup extends Component<IProps, IState> {
     this.props.navigation.goBack();
   };
 
+  validateEmail = () => {
+    const { email } = this.state;
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!re.test(String(email).toLowerCase())) 
+    this.setState({ emailError: 'Email is invalid' });
+    if (email.trim() === '') 
+    this.setState({ emailError: 'Email is required' });
+  }
+
+  validatePassword = () => {
+    const { password } = this.state;
+    if (password.length < 6) 
+    this.setState({ passwordError: 'Password must be at least 6 characters' });
+    if (password.trim() === '')
+    this.setState({ passwordError: 'Password is required' })
+  }
+
+  validateName = () => {
+    const { name } = this.state;
+    if (name.trim() === '') 
+    this.setState({ nameError: 'Name is required' });
+  }
+
   onPress = () => {}
 
   render() {
-    const {email, password, name} = this.state;
+    const {
+      email, 
+      password, 
+      name, 
+      emailError, 
+      passwordError, 
+      passwordFocus, 
+      nameError,
+      emailFocus, 
+      nameFocus
+    } = this.state;
     return (
       <View style={styles.container}>
-        <Text style={[ styles.text, styles.header ]}>Join Pop Corn!</Text>
+        <Text style={[ 
+          styles.text, 
+          styles.header
+        ]}>Join Pop Corn!</Text>
         <TextInput
-          style={[ styles.text, styles.input ]}
+          style={[ 
+            styles.text, 
+            styles.input,
+            !!nameError && styles.borderError,
+            nameFocus && styles.inputFocus
+          ]}
           placeholder="First and Last name"
           value={name}
           onChangeText={this.onChangeName}
+          onFocus={() => {
+            this.setState({ nameFocus: true, nameError: '' });
+          }}
+          onBlur={() => {
+            this.validateName();
+            this.setState({ nameFocus: false })
+          }}
         />
+        { 
+          !!nameError &&
+          <Text style={[ styles.text, styles.error ]}>{nameError}</Text>
+        }
         <TextInput
-          style={[ styles.text, styles.input ]}
+          style={[ 
+            styles.text, 
+            styles.input, 
+            !!emailError && styles.borderError,
+            emailFocus && styles.inputFocus
+          ]}
           placeholder="Email address"
           value={email}
           onChangeText={this.onChangeEmail}
+          onBlur={() => {
+            this.validateEmail();
+            this.setState({ emailFocus: false })
+          }}
+          onFocus={() => {
+            this.setState({ emailError: '', emailFocus: true });
+          }}
         />
+        {
+          !!emailError && 
+          <Text style={[ styles.text, styles.error ]}>{emailError}</Text>
+        }
         <TextInput
-          style={[ styles.text, styles.input ]}
+          style={[ 
+            styles.text, 
+            styles.input, 
+            !!passwordError && styles.borderError,
+            passwordFocus && styles.inputFocus
+          ]}
           placeholder="Password"
           secureTextEntry={true}
           value={password}
           onChangeText={this.onChangePassword}
+          onBlur={() => { 
+            this.validatePassword();
+            this.setState({ passwordFocus: false });
+          }}
+          onFocus={() => { 
+            this.setState({ passwordError: '', passwordFocus: true }); 
+          }}
+          maxLength={64}
         />
+        {
+          !!passwordError && 
+          <Text style={[ styles.text, styles.error ]}>{passwordError}</Text>
+        }
         <TouchableOpacity onPress={this.onPress}>
           <Text style={[ styles.text, styles.button ]}>Sign up</Text>
         </TouchableOpacity>
         <View style={styles.message}>
           <Text style={[ styles.text, styles.secondaryText ]}>Already have an account? </Text>
-          <TouchableOpacity onPress={this.onLogin}>
+          <TouchableOpacity onPress={this.onLogin} style={styles.linkContainer}>
             <Text style={[styles.text, styles.link]}>Login </Text>
+            <SvgUri  source={require('../../assets/general/arrow.svg')} />
           </TouchableOpacity>
         </View>
       </View>
@@ -98,7 +197,7 @@ const styles = StyleSheet.create({
   },
   input: {
     width: '80%',
-    margin: 10,
+    marginBottom: 20,
     borderColor: 'rgba(0, 0, 0, 0.11)',
     borderWidth: 1,
     padding: 12,
@@ -131,6 +230,21 @@ const styles = StyleSheet.create({
   },
   link: {
     textTransform: 'uppercase',
-    fontFamily: 'Inter-SemiBold'
+    fontFamily: 'Inter-SemiBold',
+    marginRight: 5
+  },
+  linkContainer: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  borderError: {
+    borderColor: 'rgba(255, 0, 0, .6)',
+    marginBottom: 1,
+  },
+  error: {
+    color: 'rgba(255, 0, 0, .6)',
+  },
+  inputFocus: {
+    borderColor: 'rgba(0, 0, 0, 0.4)',
   }
 });
