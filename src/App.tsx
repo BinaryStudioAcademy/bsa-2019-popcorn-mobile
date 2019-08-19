0f765d62bfa9eef7ea3d6b341c8e45133402a269
Anton Kislyi <fakeu@MacBook-Pro-Fakeu.local>
2019-08-20 00:50:47 +0300

    add spinner component

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,34 @@
 import React, { Fragment } from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  StatusBar,
+	SafeAreaView,
+	StyleSheet,
+	ScrollView,
+	View,
+	StatusBar
 } from 'react-native';
 import SvgUri from 'react-native-svg-uri';
 
+import Spinner from './components/Spinner/Spinner';
+
 const App = () => {
-  return (
-    <Fragment>
-      <StatusBar/>
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-        >
-          <View>
-            <SvgUri
-              height={150}
-              source={require('./assets/general/popcorn-logo.svg')}
-            />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </Fragment>
-  );
+	return (
+		<Fragment>
+			{/* <Spinner /> */}
+			<StatusBar />
+			<SafeAreaView>
+				<ScrollView contentInsetAdjustmentBehavior="automatic">
+					<View>
+						<SvgUri
+							height={150}
+							source={require('./assets/general/popcorn-logo.svg')}
+						/>
+					</View>
+				</ScrollView>
+			</SafeAreaView>
+		</Fragment>
+	);
 };
 
-const styles = StyleSheet.create({
-});
+const styles = StyleSheet.create({});
 
 export default App;
