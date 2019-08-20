80bf41fa5b5bfd8ec2dbbc145c39f62929d0c818
Alexey Tryapko <aatryapko1201@gmail.com>
2019-08-20 20:50:31 +0300

    survey-story: rebese onto dev

--- src/containers/Root/root.tsx
+++ src/containers/Root/root.tsx
@@ -1,14 +1,12 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
 import { Provider } from 'react-redux'
 import { Store } from 'redux'
-import {
-    SafeAreaView,
-    ScrollView,
-    View,
-    StatusBar,
-} from 'react-native';
-import SvgUri from 'react-native-svg-uri';
+import { HomeNavigator } from './../../routes/';
+import Swiper from 'react-native-swiper';
 import StoryComponent from './../../components/MainPage/Story/index';
+import HomeComponent from './../../views/HomeView';
+import SidebarView from './../../views/SidebarView';
+import Spinner from './../../components/Spinner/Spinner';
 
 interface IProps {
     store: Store
@@ -18,7 +16,11 @@ class Root extends Component<IProps> {
     render() {
         return (
             <Provider store={this.props.store}>
-                <StoryComponent/>
+                <Swiper loop={false} showsPagination={false} index={0}>
+                    {/* <Spinner /> */}
+                    <HomeNavigator />
+                    <SidebarView />
+                </Swiper>
             </Provider>
         )
     }
