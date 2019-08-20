09a3c0f9262d438e40e7a14be955146cbd0c1939
Anton Kislyi <fakeu@MacBook-Pro-Fakeu.local>
2019-08-21 02:10:09 +0300

    add header to menynavigations

--- src/routes/StackNavigator/MenuNavigators.tsx
+++ src/routes/StackNavigator/MenuNavigators.tsx
@@ -8,16 +8,13 @@ import Home from '../../views/HomeView';
 import Messages from '../../views/MessagesView';
 import Events from '../../views/EventsView';
 import Collections from '../../views/CollectionsView';
+import Header from '../../components/Header/Header';
 
 export const FirstActivity_StackNavigator = createStackNavigator({
 	First: {
 		screen: Home,
 		navigationOptions: ({ navigation }) => ({
-			title: 'Home',
-			headerStyle: {
-				backgroundColor: '#FF9800'
-			},
-			headerTintColor: '#fff'
+			header: Header
 		})
 	}
 });
@@ -26,11 +23,7 @@ export const Messages_StackNavigator = createStackNavigator({
 	Second: {
 		screen: Messages,
 		navigationOptions: ({ navigation }) => ({
-			title: 'Messages',
-			headerStyle: {
-				backgroundColor: '#FF9800'
-			},
-			headerTintColor: '#fff'
+			header: Header
 		})
 	}
 });
@@ -39,11 +32,7 @@ export const Events_StackNavigator = createStackNavigator({
 	Third: {
 		screen: Events,
 		navigationOptions: ({ navigation }) => ({
-			title: 'Events',
-			headerStyle: {
-				backgroundColor: '#FF9800'
-			},
-			headerTintColor: '#fff'
+			header: Header
 		})
 	}
 });
@@ -52,11 +41,7 @@ export const Collections_StackNavigator = createStackNavigator({
 	Fourth: {
 		screen: Collections,
 		navigationOptions: ({ navigation }) => ({
-			title: 'Collections',
-			headerStyle: {
-				backgroundColor: '#FF9800'
-			},
-			headerTintColor: '#fff'
+			header: Header
 		})
 	}
 });
