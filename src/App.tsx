d836833e9b00e4797879c22098a10fd6ee16b8f0
vshreder <verashreder@gmail.com>
2019-08-20 15:08:36 +0300

    feature/navigation: merge to dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
-import Router from './routes/';
+import { HomeNavigator } from './routes/';
 import React, { Component } from 'react';
-import { createStackNavigator } from 'react-navigation';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
 import HomeComponent from './views/HomeView';
-import Page1 from './views/SidebarView';
 import Swiper from 'react-native-swiper';
 import SidebarView from './views/SidebarView';
+import {
+	SafeAreaView,
+	StyleSheet,
+	ScrollView,
+	View,
+	StatusBar
+} from 'react-native';
+import styles from '../assets/style';
 
-export default class CustomDrawer extends Component {
+import Spinner from './components/Spinner/Spinner';
+export default class App extends Component {
 	render() {
 		return (
 			<Swiper loop={false} showsPagination={false} index={0}>
-				<Router />
+				{/* <Spinner /> */}
+				<HomeNavigator />
 				<SidebarView />
 			</Swiper>
 		);
 	}
 }
+
+export default App;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,42 +1,6 @@
-import {
-	createAppContainer,
-	createStackNavigator,
-	createDrawerNavigator
-} from 'react-navigation';
-import {
-	FirstActivity_StackNavigator,
-	Messages_StackNavigator,
-	Events_StackNavigator,
-	Collections_StackNavigator
-} from './MenuNavigators.tsx';
+import AuthNavigator from './StackNavigator/AuthNavigator';
+import AppNavigator from './SwitchNavigator/AppNavigator';
+import HomeNavigator from './StackNavigator/HomeNavigator';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
 
-const DrawerNavigatorExample = createDrawerNavigator({
-	Home: {
-		screen: FirstActivity_StackNavigator,
-		navigationOptions: {
-			drawerLabel: 'Home'
-		}
-	},
-
-	Messages: {
-		screen: Messages_StackNavigator,
-		navigationOptions: {
-			drawerLabel: 'Messages'
-		}
-	},
-
-	Events: {
-		screen: Events_StackNavigator,
-		navigationOptions: {
-			drawerLabel: 'Events'
-		}
-	},
-	Collections: {
-		screen: Collections_StackNavigator,
-		navigationOptions: {
-			drawerLabel: 'Collections'
-		}
-	}
-});
-
-export default createAppContainer(DrawerNavigatorExample);
+export { AuthNavigator, AppNavigator, HomeNavigator };
