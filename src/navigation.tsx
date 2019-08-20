52b987edbd5f8561e2bb80db6d462641fa96adea
Anton Kislyi <fakeu@MacBook-Pro-Fakeu.local>
2019-08-20 11:18:50 +0300

    remove test files

--- src/navigation.tsx
+++
@@ -1,9 +0,0 @@
-import { createStackNavigator, createAppContainer } from 'react-navigation';
-
-import Profile from './screens/Profile';
-
-const Test = createStackNavigator({
-	Prof: Profile
-});
-
-export default createAppContainer(Test);
