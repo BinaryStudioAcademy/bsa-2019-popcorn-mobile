32ba5b536e0e6222bb262db4e9de647d1e715fa0
Uliana Lobanova <lobanova.ulianaa@gmail.com>
2019-08-21 00:38:04 +0300

    content-page: connect login and content with home navigator

--- src/containers/Main/Main.tsx
+++ src/containers/Main/Main.tsx
@@ -6,12 +6,10 @@ import { View } from 'react-native';
 
 const Main: React.FC = () => {
 	return (
-		<View style={{flex: 1}}>
 		<Swiper loop={false} showsPagination={false} index={0}>
 			<HomeNavigator />
 			<SidebarView />
 		</Swiper>
-		</View>
 	);
 };
 
