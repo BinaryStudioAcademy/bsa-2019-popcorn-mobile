b0ba87eaf243b713721406636a7fa8690fcbf5ef
Alexey Tryapko <aatryapko1201@gmail.com>
2019-08-21 01:18:34 +0300

    post: connect post to root store

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import storyReducer from './../components/MainPage/Story/reducer';
 import authReducer from '../components/Authorization/reducer';
+import postReducer from '../components/MainPage/Post/reducer';
 
 const reducers = {
 	story: storyReducer,
+	post: postReducer,
 };
 
 export default combineReducers({
