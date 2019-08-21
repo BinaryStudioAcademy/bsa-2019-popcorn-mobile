b2dee622bac2e19edcce4d364bd3264c84a4dc60
Alexey Tryapko <aatryapko1201@gmail.com>
2019-08-21 03:41:35 +0300

    film-page: setup redux

---
+++ src/components/MainPage/Movie/sagas.ts
@@ -0,0 +1,26 @@
+import { fetchMovies } from './../../../redux/routines';
+import { call, put, takeEvery, all } from 'redux-saga/effects';
+import * as movieService from './../../../services/movie.service';
+
+export function* getMovies() {
+    try {
+        yield put(fetchMovies.request());
+        const response = yield call(movieService.getAllMovies);
+
+        yield put(fetchMovies.success(response));
+    } catch (error) {
+        yield put(fetchMovies.failure(error.message));
+    } finally {
+        yield put(fetchMovies.fulfill());
+    }
+}
+
+function* watchGetMovies() {
+    yield takeEvery(fetchMovies.TRIGGER, getMovies)
+}
+
+export default function* messagesSaga() {
+    yield all([
+        watchGetMovies(),
+    ])
+};
