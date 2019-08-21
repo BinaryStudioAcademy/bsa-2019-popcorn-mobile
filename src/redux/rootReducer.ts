55b1d1d12121188a603add76f4db63da963bbf8b
Alexey Tryapko <aatryapko1201@gmail.com>
2019-08-21 03:42:25 +0300

    film-page: connect to root store

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import storyReducer from './../components/MainPage/Story/reducer';
 import authReducer from '../components/Authorization/reducer';
+import movieReducer from '..//components/MainPage/Movie/reducer';
 
 const reducers = {
 	story: storyReducer,
+	movie: movieReducer,
 };
 
 export default combineReducers({
--- src/redux/saga.ts
+++ src/redux/saga.ts
@@ -1,10 +1,12 @@
 import { all } from 'redux-saga/effects';
 import authSaga from '../components/Authorization/saga';
 import StorySaga from './../components/MainPage/Story/sagas';
+import MovieSaga from './../components/MainPage/Movie/sagas';
 
 export default function* rootSaga() {
 	yield all([
 		StorySaga(),
-		authSaga()
+		authSaga(),
+		MovieSaga(),
 	]);
 }
