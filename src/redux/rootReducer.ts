80a6f51f50b63a11d4a9c481ba36757ad66feba6
Alexey Tryapko <aatryapko1201@gmail.com>
2019-08-22 13:59:35 +0300

    film-page: connect movie to root store

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import storyReducer from './../components/MainPage/Story/reducer';
 import authReducer from '../components/Authorization/reducer';
-import movieReducer from '..//components/MainPage/Movie/reducer';
+import moviesReducer from '../components/MainPage/Movie/reducer';
 import postReducer from '../components/MainPage/Post/reducer';
+import movieReducer from '../components/MainPage/Movie/Movie/reducer';
 
 const reducers = {
 	story: storyReducer,
+	movies: moviesReducer,
 	movie: movieReducer,
 	post: postReducer,
 };
--- src/redux/saga.ts
+++ src/redux/saga.ts
@@ -1,14 +1,16 @@
 import { all } from 'redux-saga/effects';
 import authSaga from '../components/Authorization/saga';
 import StorySaga from './../components/MainPage/Story/sagas';
-import MovieSaga from './../components/MainPage/Movie/sagas';
+import MoviesSaga from './../components/MainPage/Movie/sagas';
 import PostSaga from './../components/MainPage/Post/sagas';
+import MovieSaga from './../components/MainPage/Movie/Movie/saga';
 
 export default function* rootSaga() {
 	yield all([
 		StorySaga(),
 		authSaga(),
-		MovieSaga(),
+		MoviesSaga(),
 		PostSaga(),
+		MovieSaga(),
 	]);
 }
