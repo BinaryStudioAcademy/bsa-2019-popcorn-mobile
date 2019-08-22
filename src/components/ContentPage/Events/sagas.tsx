2e890ce129373b7304da9d99814fc75fbbc7646a
Uliana Lobanova <lobanova.ulianaa@gmail.com>
2019-08-22 16:32:49 +0300

    event-list: implemented event list

---
+++ src/components/ContentPage/Events/sagas.tsx
@@ -0,0 +1,100 @@
+import { all, call, put, takeEvery } from 'redux-saga/effects';
+import { 
+    fetchEvents, 
+    createEventVisitor, 
+    updateEventVisitor, 
+    deleteEventVisitor 
+} from '../../../redux/routines';
+import config from '../../../config';
+import webApi from '../../../helpers/webApi.helper';
+
+function* getEvents(action) {
+    try {
+        yield put(fetchEvents.request());
+        const data = yield call(webApi, {
+			endpoint: config.API_URL + '/api/event',
+			method: 'GET'
+        });
+        yield put(fetchEvents.success(data));
+    } catch(e) {
+        yield put(fetchEvents.failure(e.message));
+    } finally {
+        yield put(fetchEvents.fulfill());
+    }
+} 
+
+function* watchFetchEvents() {
+    yield takeEvery(fetchEvents.trigger, getEvents);
+}
+
+function* createVisitor(action) {
+    try {
+        yield put(createEventVisitor.request());
+        const data = yield call(webApi, {
+            endpoint: config.API_URL + '/api/event/visitor',
+            method: 'POST',
+            body: {
+                ...action.payload
+            }
+        });
+        yield put(fetchEvents.trigger());
+    } catch(e) {
+        yield put(createEventVisitor.failure(e.message));
+    } finally {
+        yield put(createEventVisitor.fulfill());
+    }
+}
+
+function* watchCreateVisitor() {
+    yield takeEvery(createEventVisitor.trigger, createVisitor);
+}
+
+function* updateVisitor(action) {
+    try {
+        yield put(updateEventVisitor.request());
+        const data = yield call(webApi, {
+            endpoint: config.API_URL + '/api/event/visitor',
+            method: 'PUT',
+            body: {
+                ...action.payload
+            }
+        });
+        yield put(fetchEvents.trigger());
+    } catch(e) {
+        yield put(updateEventVisitor.failure(e.message));
+    } finally {
+        yield put(updateEventVisitor.fulfill());
+    }
+}
+
+function* watchUpdateVisitor() {
+    yield takeEvery(updateEventVisitor.trigger, updateVisitor);
+}
+
+function* deleteVisitor(action) {
+    try {
+        yield put(deleteEventVisitor.request());
+        const data = yield call(webApi, {
+            endpoint: config.API_URL + '/api/event/visitor/' + action.payload,
+            method: 'DELETE'
+        });
+        yield put(fetchEvents.trigger());
+    } catch(e) {
+        yield put(deleteEventVisitor.failure(e.message));
+    } finally {
+        yield put(deleteEventVisitor.fulfill());
+    }
+}
+
+function* watchDeleteVisitor() {
+    yield takeEvery(deleteEventVisitor.trigger, deleteVisitor);
+}
+
+export default function* eventsSaga() {
+    yield all([
+        watchFetchEvents(), 
+        watchCreateVisitor(), 
+        watchDeleteVisitor(), 
+        watchUpdateVisitor()
+    ]);
+}
--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 import { APP_API_URL } from 'react-native-dotenv'
 const config = {
-	API_URL: APP_API_URL || 'http://192.168.0.191:5000',
+	API_URL: APP_API_URL || 'http://192.168.0.105:5000',
 	DEFAULT_AVATAR: '/images/default/avatar-default.png',
-	DEFAULT_EVENT_IMAGE: '/images/default/event-default.png',
+	DEFAULT_EVENT_IMAGE: '/src/assets/general/event.jpg',
 	DEFAULT_MOVIE_IMAGE: '/images/default/movie-default.png',
 	POSTER_PATH: 'https://image.tmdb.org/t/p/w500'
 };
