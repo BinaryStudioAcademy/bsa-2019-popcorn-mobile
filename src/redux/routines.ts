bd7aac6e1d6cd555b181cef2d88c79fa5b0ddd69
Alexey Tryapko <aatryapko1201@gmail.com>
2019-08-22 13:59:35 +0300

    film-page: connect movie to root store

--- src/redux/routines.ts
+++ src/redux/routines.ts
@@ -5,5 +5,6 @@ export const register = createRoutine('REGISTER');
 export const fetchUser = createRoutine('FETCH_USER');
 export const fetchStories = createRoutine('FETCH_STORIES');
 export const fetchStory  = createRoutine('FETCH_STORY');
-export const fetchMovies = createRoutine('FETCH_MOVIE');
+export const fetchMovies = createRoutine('FETCH_MOVIES');
 export const fetchPosts = createRoutine('FETCH_POSTS');
+export const fetchMovie = createRoutine('FETCH_MOVIE');
