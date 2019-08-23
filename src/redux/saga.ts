42638e009833ce2046bc48f0eaf02b2586cb6829
Uliana Lobanova <lobanova.ulianaa@gmail.com>
2019-08-23 12:55:34 +0300

    event-page: fixed event page bugs

--- src/redux/saga.ts
+++ src/redux/saga.ts
@@ -3,7 +3,7 @@ import authSaga from '../components/Authorization/saga';
 import StorySaga from './../components/MainPage/Story/sagas';
 import MoviesSaga from './../components/MainPage/Movie/sagas';
 import PostSaga from './../components/MainPage/Post/sagas';
-import EventSaga from '../components/UserPage/Events/sagas';
+import EventSaga from '../components/ContentPage/Events/sagas';
 import survey from '../components/ContentPage/Surveys/saga';
 import UserEventsSaga from '../components/UserPage/Events/sagas';
 import MovieSaga from './../components/MainPage/Movie/Movie/saga';
