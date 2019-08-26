2533e595c60d2a94163c7ae54be0607c034ba618
igor185 <igor.babin185@gmail.com>
2019-08-26 18:21:59 +0300

    post-constructor: add simple view and create send post flow

---
+++ src/components/MainPage/Post/Constructor/styles.ts
@@ -0,0 +1,27 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+	input: {
+		width: '80%',
+		margin: 20,
+		borderColor: 'rgba(0, 0, 0, 0.11)',
+		borderWidth: 1,
+		padding: 12,
+		paddingLeft: 15,
+		fontSize: 16,
+		borderRadius: 3
+	},
+	iconsWrp: {
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'center',
+		margin: 20
+	},
+	roundImage: {
+		width: '80%',
+		height: 200,
+		borderRadius: 20,
+		margin: 9,
+		backgroundColor: '#adadad'
+	}
+});
