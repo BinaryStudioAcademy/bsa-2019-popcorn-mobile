826ba88bd11cdb45d3bd20e464df9ef9a6e16d8b
Uliana Lobanova <lobanova.ulianaa@gmail.com>
2019-08-26 23:17:03 +0300

    notifications: implemented notification view

--- src/routes/StackNavigator/MenuNavigators.tsx
+++ src/routes/StackNavigator/MenuNavigators.tsx
@@ -3,13 +3,11 @@ import Home from '../../views/HomeView';
 import Messages from '../../views/MessagesView';
 import Collections from '../../views/CollectionsView';
 import Header from '../../components/Header/Header';
-import EventList from '../../components/ContentPage/Events/EventList';
-import SurveyList from '../../components/ContentPage/Surveys/SurveyList';
-import TopList from '../../components/ContentPage/Tops/TopList';
 import SurveyNavigator from '../StackNavigator/SurveyNavigator';
 import EventsTab from '../../components/ContentPage/Events/EventTab';
 import MovieNavigator from './MovieNavigator';
 import TopsTab from '../../components/ContentPage/Tops/TopTab';
+import Notifications from '../../components/Notifications/NotificationList';
 
 export const FirstActivity_StackNavigator = createStackNavigator({
 	First: {
@@ -68,6 +66,15 @@ export const Collections_StackNavigator = createStackNavigator({
 export const Movies_StackNavigator = createStackNavigator({
 	Fourth: {
 		screen: MovieNavigator,
+		navigationOptions: ({ navigation }) => ({
+			header: Header
+		})
+	}
+});
+
+export const Notifications_Navigator = createStackNavigator({
+	Seventh: {
+		screen: Notifications,
 		navigationOptions: ({ navigation }) => ({
 			header: Header
 		})
