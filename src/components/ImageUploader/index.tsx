33653369c6dc3cdae15adb862de4655faf970c05
igor185 <igor.babin185@gmail.com>
2019-08-27 13:53:35 +0300

    post-constructor: some fix with upload photo

--- src/components/ImageUploader/index.tsx
+++ src/components/ImageUploader/index.tsx
@@ -2,26 +2,28 @@ import ImagePicker from 'react-native-image-picker';
 import React from 'react';
 import { TouchableOpacity } from 'react-native';
 import SvgUri from 'react-native-svg-uri';
+import { uploadFile } from '../../services/file.service';
 
 interface IProps {
-	saveUrl: (url: string) => void;
+	saveUrl: (url: string) => any;
 	src: any;
 }
 
 class ImageUploader extends React.Component<IProps> {
-	showPicker() {
+	showPicker(saveUrl: (url: string) => any) {
 		ImagePicker.showImagePicker({}, response => {
-			if (response.didCancel || !response.data) return;
-			const file = response.data;
+			if (response.didCancel || !response.data) return JSON.stringify(response);
 
-			const data = new FormData();
-			data.append('file', file);
+			uploadFile(response.data, response.type || '')
+				.then(saveUrl)
+				.catch(e => alert(JSON.stringify(e.message)));
 		});
 	}
 
 	render() {
+		const { saveUrl } = this.props;
 		return (
-			<TouchableOpacity onPress={this.showPicker}>
+			<TouchableOpacity onPress={() => this.showPicker(saveUrl)}>
 				<SvgUri height={48} width={48} source={this.props.src} />
 			</TouchableOpacity>
 		);
--- src/services/file.service.ts
+++ src/services/file.service.ts
@@ -1,15 +1,19 @@
 import config from '../config';
 import { Storage } from '../helpers/storage.helper';
+import RNFetchBlob from 'react-native-fetch-blob';
 
-export const uploadFile = async (file: FormData) => {
+export const uploadFile = async (base64ImageData: string, format: string) => {
 	const token = await Storage.get('token');
-	let response = await fetch(`${config.API_URL}/api/image/upload`, {
-		headers: {
-			Authorization: `Bearer ${token}`
+	let imgSrc = 'data:image/png;base64,' + base64ImageData;
+	const response = await RNFetchBlob.fetch(
+		'POST',
+		`${config.API_URL}/api/image/upload`,
+		{
+			Authorization: `Bearer ${token}`,
+			'Content-Type': 'multipart/form-data'
 		},
-		method: 'POST',
-		body: file
-	});
+		[{ name: 'image', filename: 'img' + format, data: imgSrc }]
+	);
 	const { imageUrl } = await response.json();
 
 	let url;
