bdb98d4044f6ac004bbb1a638d676faaa6661a03
Anton Kislyi <fakeu@MacBook-Pro-Fakeu.local>
2019-08-30 19:01:17 +0300

    fix servey page

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 import { APP_API_URL } from 'react-native-dotenv';
 const config = {
-	API_URL: 'https://popcorn-staging.herokuapp.com',
+	API_URL: 'http://localhost:5000',
 	DEFAULT_AVATAR: '/images/default/avatar-default.png',
 	DEFAULT_EVENT_IMAGE: '/src/assets/general/event.jpg',
 	DEFAULT_MOVIE_IMAGE: '/images/default/movie-default.png',
