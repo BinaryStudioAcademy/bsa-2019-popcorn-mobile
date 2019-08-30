89f44adbbe12e7c7597efc1b0a772cc9e359d9c0
vshreder <verashreder@gmail.com>
2019-08-30 13:26:04 +0300

    fix imports

--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -73,7 +73,7 @@ const HomeView = ({ navigation }) => {
 				)}
 			>
 				<View style={{ flex: 1 }}>
-					<PostCompomonent />
+					<PostComponent />
 				</View>
 			</ParallaxScrollView>
 		</View>
