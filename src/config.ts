b6b709d6b18ec24e636ec4952d29272b32558c87
Uliana Lobanova <lobanova.ulianaa@gmail.com>
2019-09-02 05:26:04 +0300

    activity-page: remove ip from api url

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 import { APP_API_URL } from 'react-native-dotenv';
 const config = {
-	API_URL: 'http://192.168.1.103:5000',
+	API_URL: 'https://bsa-popcorn.herokuapp.com/',
 	DEFAULT_AVATAR: '/images/default/avatar-default.png',
 	DEFAULT_EVENT_IMAGE: '/src/assets/general/event.jpg',
 	DEFAULT_MOVIE_IMAGE: '/images/default/movie-default.png',
