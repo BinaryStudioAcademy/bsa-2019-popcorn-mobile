4658d75e3fd7db43bd7361df79df0197f2575824
Uliana Lobanova <lobanova.ulianaa@gmail.com>
2019-09-05 13:59:03 +0300

    collections: fetch details about list

--- src/components/Collections/Collection.tsx
+++ src/components/Collections/Collection.tsx
@@ -1,19 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import {
+    fetchCollectionDetails
+} from '../../redux/routines';
+import Spinner from '../Spinner/Spinner';
+import { IMovieListDetails } from './ICollection';
 
 interface IProps {
-    navigation: any
+    navigation: any;
+    loading: boolean;
+    collection: IMovieListDetails;
+    fetchCollectionDetails: ({ movieListId: string }) => void
 }
 
-const Collection: React.FC<IProps> = ({ navigation }) => {
-    return (
-        <View>
-            <Text>{navigation.state.params.id}</Text>
-            <TouchableOpacity onPress={() => { navigation.goBack() }}>
-                <Text>Go back</Text>
-            </TouchableOpacity>
-        </View>
-    )
+class Collection extends Component<IProps> {
+    componentDidMount() {
+        this.props.fetchCollectionDetails({ movieListId: this.props.navigation.state.params.id });
+    }
+
+    render () {
+        const { navigation, loading, collection } = this.props;
+        if (loading || !this.props.collection) return <Spinner />
+        return (
+            <View>
+                <Text>{collection.movieList.title}</Text>
+                <TouchableOpacity onPress={() => { navigation.goBack() }}>
+                    <Text>Go back</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
 }
 
-export default Collection;
+const mapStateToProps = (rootState, props) => ({
+    ...props,
+    loading: rootState.collections.loading,
+	collection: rootState.collections.movieListDetails,
+});
+
+const actions = {
+    fetchCollectionDetails
+}
+
+const mapDispatchToProps = dispatch => bindActionCreators(actions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Collection);
