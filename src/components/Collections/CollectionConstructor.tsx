6b0c5efa022761815ca174d2b9add2de20e9dc0e
Uliana Lobanova <lobanova.ulianaa@gmail.com>
2019-09-05 13:59:03 +0300

    collections: fetch details about list

--- src/components/Collections/CollectionConstructor.tsx
+++ src/components/Collections/CollectionConstructor.tsx
@@ -1,19 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import {
+    saveCollection
+} from '../../redux/routines';
+import Spinner from '../Spinner/Spinner';
+import { INewMovieList } from './ICollection';
 
 interface IProps {
-    navigation: any
+    navigation: any;
+    saveCollection: ({ movieList: INewMovieList }) => void
 }
 
-const CollectionConstructor: React.FC<IProps> = ({ navigation }) => {
-    return (
-        <View>
-            <Text>constructor</Text>
-            <TouchableOpacity onPress={() => { navigation.goBack() }}>
-                <Text>Save</Text>
-            </TouchableOpacity>
-        </View>
-    )
+interface IState {
+   collection: INewMovieList 
 }
 
-export default CollectionConstructor;
+class Collection extends Component<IProps, IState> {
+    constructor(props) {
+        super(props);
+    }
+
+    render () {
+        const { navigation } = this.props.navigation;
+        return (
+            <View>
+                <Text>constructor</Text>
+                <TouchableOpacity onPress={() => { navigation.goBack() }}>
+                    <Text>Go back</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+}
+
+const mapStateToProps = (rootState, props) => ({
+    ...props,
+    loading: rootState.collections.loading,
+	collection: rootState.collections.movieListDetails,
+});
+
+const actions = {
+    saveCollection
+}
+
+const mapDispatchToProps = dispatch => bindActionCreators(actions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Collection);
--- src/components/Collections/ICollection.tsx
+++ src/components/Collections/ICollection.tsx
@@ -7,3 +7,46 @@ export interface IMovieListPreview {
 	moviesId: string[];
 	createdAt: Date;
 }
+
+export interface IMovie {
+	id: string;
+	title: string;
+	poster_path: string;
+	release_date: string;
+	runtime: number;
+	overview: string;
+	genres: string[];
+	watchInfo?: IWatchListId;
+}
+
+export interface IWatchListId {
+	id: string;
+	movieId: string;
+	status: string;
+}
+
+export interface IMovieListDetails {
+	movieList: {
+		id: string;
+		title: string;
+		description: string;
+		isPrivate: boolean;
+		imageUrl: string;
+		moviesId: string[];
+		createdAt: Date;
+		user: {
+			id: string;
+			name: string;
+			avatar: string;
+		};
+	};
+	movies: IMovie[];
+}
+
+export interface INewMovieList {
+	title: string;
+	isPrivate: boolean;
+	despription?: string;
+	imageUrl?: string;
+	moviesId: string[];
+}
