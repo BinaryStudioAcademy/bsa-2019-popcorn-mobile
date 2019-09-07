af941ff5b6675bf27c3f18690b1ef3f90cda27ca
Uliana Lobanova <lobanova.ulianaa@gmail.com>
2019-09-07 19:40:39 +0300

    notifications-improvements: implement redirect on new message and follower

--- src/routes/StackNavigator/NotificationNavigator.tsx
+++ src/routes/StackNavigator/NotificationNavigator.tsx
@@ -1,6 +1,8 @@
 import { createStackNavigator } from 'react-navigation';
 import Main from '../../containers/Main/Main';
 import EventPage from '../../components/ContentPage/Events/EventPage';
+import { UserPage_StackNavigator } from './MenuNavigators';
+import Messages from '../../views/Messages/Messages';
 
 const NotificationNavigator = createStackNavigator({
 	Main: {
@@ -11,6 +13,18 @@ const NotificationNavigator = createStackNavigator({
 	},
 	Event: {
 		screen: EventPage,
+		navigationOptions: {
+			header: null
+		}
+	},
+	Follower: {
+		screen: UserPage_StackNavigator,
+		navigationOptions: {
+			header: null
+		}
+	},
+	Message: {
+		screen: Messages,
 		navigationOptions: {
 			header: null
 		}
